eb196a8bb365a9cb8cdfd491b9a3700ba1b65dd5
balex <balexvicx@gmail.com>
2024-04-04 20:48:06 +0200

    sealed class for state

--- app/src/main/java/com/balex/factorial/State.kt
+++ app/src/main/java/com/balex/factorial/State.kt
@@ -1,7 +1,7 @@
 package com.balex.factorial
 
-class State(
-    val isError: Boolean = false,
-    val isInProgress: Boolean = false,
-    val factorial: String = ""
-)
+sealed class State
+
+data object Error : State()
+data object Progress : State()
+class Result(val factorial: String) : State()
